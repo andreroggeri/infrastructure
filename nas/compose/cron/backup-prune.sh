3f811d15490cfc354b6b13cee3035717aa48a62d
André Roggeri Campos <a.roggeri.c@gmail.com>
2023-05-08 14:50:53 -0300

    fix: Backup concurrency

--- nas/compose/cron/backup-prune.sh
+++ nas/compose/cron/backup-prune.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+sleep 120 # Wait for backup to finish
+
 restic forget \
     --keep-last 5 \
     --keep-monthly 2 \
     --repo "${RESTIC_REPOSITORY}" \
     --password-command "echo ${RESTIC_PASSWORD}" \
+    --prune \
     --cache-dir /cache
