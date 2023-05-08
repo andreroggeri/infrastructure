615dbd786f2ec87a476f22854f5934b36e1401a2
André Roggeri Campos <a.roggeri.c@gmail.com>
2023-05-08 09:56:51 -0300

    chore: Prune old backups

---
+++ nas/compose/cron/prune-backups.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+restic forget \
+    --keep-last 5 \
+    --keep-monthly 2 \
+    --repo "${RESTIC_REPOSITORY}" \
+    --password-command "echo ${RESTIC_PASSWORD}" \
+    --cache-dir /cache
